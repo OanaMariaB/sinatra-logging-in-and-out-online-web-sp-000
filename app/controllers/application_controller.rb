871a43cc7beb7412c4be935a956b5446e12a23fd
Oana Berentan <oana.berentan@yahoo.com>
2020-01-06 22:00:29 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,8 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     @user = User.find_by(username:params["username"], password: params["password"])
-    if session[:user_id] = @user.id
+    if user
+      session[:user_id] = @user.id
      redirect "/account"
     else
      erb :error
