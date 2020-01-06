214e044982f366fe90377d449efa7fad8f20e644
Oana Berentan <oana.berentan@yahoo.com>
2020-01-06 21:26:06 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -2,5 +2,7 @@ class Helpers
 
   def self.current_user(session)
     @user= User.create(username: params["username"], password:params["password"], balance:params["balance"])
+    @user.save
+    @user
   end
 end
